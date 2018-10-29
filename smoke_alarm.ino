45f5da88c83d9b3219d96013adad37b1af9b2826
besujju <44573183+besujju@users.noreply.github.com>
2018-10-29 18:25:06 +0530

    Add files via upload

---
+++ smoke_alarm.ino
@@ -0,0 +1,35 @@
+int val = 0;  
+void setup()
+{
+pinMode(2, INPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(5, OUTPUT);
+digitalWrite(3, HIGH);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+Serial.begin(9600);
+Serial.print("LOW");
+delay(2000);
+}
+void loop() 
+{
+val = digitalRead(2);
+if(val==0)
+{       
+digitalWrite(3,LOW );  
+digitalWrite(4, HIGH);
+digitalWrite(5, HIGH);   
+Serial.print("HIGH");
+delay(2000);
+ }
+if(val==0)
+{       
+digitalWrite(3, HIGH);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+Serial.print("LOW");
+}
+}
+
+
